41ad961a22fd080c7e376fffbe59bfe6acb54dd9
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-02-25 17:37:11 +0700

    Created a Custom Form

--- Day 4/mysite/main/urls.py
+++ Day 4/mysite/main/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:name>', views.index, name='index'),
+    path('<int:id>', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('create/', views.create, name='create')
 ]
