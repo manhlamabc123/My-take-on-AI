f8d31ff0bcfee473978f27eebd4b06f95f7c185f
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-03-02 14:52:41 +0700

    Added Game object to Database

--- Day 5 - 9/mysite/main/models.py
+++ Day 5 - 9/mysite/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
