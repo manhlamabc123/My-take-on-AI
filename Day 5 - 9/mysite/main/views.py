b6c50c6da0d659d4b96b8a34003068fc35be8866
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-03-02 18:34:54 +0700

    Added html files and Bootstrap

--- Day 5 - 9/mysite/main/views.py
+++ Day 5 - 9/mysite/main/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(response):
-    return HttpResponse("Hello world.")
+def home(response):
+    return render(response, "main/home.html", {})
