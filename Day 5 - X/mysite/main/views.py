158b5e6405747fb6f96afb6c45f3ee769099e1dd
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-03-03 19:41:29 +0700

    Update Personal Page

--- Day 5 - X/mysite/main/views.py
+++ Day 5 - X/mysite/main/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Member
 
 # Create your views here.
 def home(response):
@@ -8,7 +9,8 @@ def home(response):
 
 def profile(response):
     user = response.user
-    return render(response, "main/profile.html", {"user": user})
+    member = Member.objects.get(user_id = user.id)
+    return render(response, "main/profile.html", {"user": user, "member": member})
 
 def edit_profile(response):
     return render(response, "main/edit_profile.html", {})
